915c9f73be04a3f35aa9ce5238822d37526736be
Fiach Antaw <fiach.antaw@uqconnect.edu.au>
2017-03-09 18:35:05 +1000

    Add IIO sink block for output devices

---
+++ IIOSink.cpp
@@ -0,0 +1,165 @@
+// Copyright (c) 2016 Fiach Antaw
+// SPDX-License-Identifier: BSL-1.0
+
+#include <Poco/Error.h>
+#include <memory>
+#include <string>
+#include <cstring>
+#include "IIOSupport.hpp"
+
+/***********************************************************************
+ * |PothosDoc IIO Sink
+ *
+ * The IIO source forwards an input sample stream to an IIO output device.
+ *
+ * |category /IIO
+ * |category /Sinks
+ * |keywords iio industrial io adc sdr
+ *
+ * |param deviceId[Device ID] The ID of an IIO device on the system.
+ * |widget StringEntry()
+ * |default ""
+ *
+ * |factory /iio/sink(deviceId)
+ **********************************************************************/
+class IIOSink : public Pothos::Block
+{
+private:
+    std::unique_ptr<IIODevice> dev;
+    std::unique_ptr<IIOBuffer> buf;
+public:
+    IIOSink(const std::string &deviceId)
+    {
+        //get libiio context
+        IIOContext& ctx = IIOContext::get();
+        
+        //find iio device
+        for (auto d : ctx.devices())
+        {
+            if (d.id() == deviceId)
+            {
+                this->dev = std::unique_ptr<IIODevice>(new IIODevice(d));
+                break;
+            }
+        }
+        if (!this->dev)
+        {
+            throw Pothos::SystemException("IIOSink::IIOSink()", "device not found");
+        }
+
+        //set up probes/setters for device attributes 
+        for (auto a : this->dev->attributes())
+        {
+            Pothos::Callable attrGetter(&IIOSink::getDeviceAttribute);
+            Pothos::Callable attrSetter(&IIOSink::setDeviceAttribute);
+            attrGetter.bind(std::ref(*this), 0);
+            attrGetter.bind(a, 1);
+            attrSetter.bind(std::ref(*this), 0);
+            attrSetter.bind(a, 1);
+
+            std::string getDeviceAttrName = "deviceAttribute[" + a.name() + "]";
+            std::string setDeviceAttrName = "setdeviceAttribute[" + a.name() + "]";
+            this->registerCallable(getDeviceAttrName, attrGetter);
+            this->registerCallable(setDeviceAttrName, attrSetter);
+            this->registerProbe(getDeviceAttrName);
+        }
+
+        //disable all output channels/enable all input channels
+        bool have_scan_elements = false;
+        for (auto c : this->dev->channels())
+        {
+            if (c.isOutput())
+            {
+                c.enable();
+
+                //set up input ports for scannable input channels
+                if (c.isScanElement())
+                {
+                    this->setupInput(c.id(), c.dtype());
+                    have_scan_elements = true;
+                }
+
+                //set up probes/setters for channel attributes
+                for (auto a : c.attributes())
+                {
+                    Pothos::Callable attrGetter(&IIOSink::getChannelAttribute);
+                    Pothos::Callable attrSetter(&IIOSink::setChannelAttribute);
+                    attrGetter.bind(std::ref(*this), 0);
+                    attrGetter.bind(a, 1);
+                    attrSetter.bind(std::ref(*this), 0);
+                    attrSetter.bind(a, 1);
+
+                    std::string getChannelAttrName = "channelAttribute[" + c.id() + "][" + a.name() + "]";
+                    std::string setChannelAttrName = "setChannelAttribute[" + c.id() + "][" + a.name() + "]";
+                    this->registerCallable(getChannelAttrName, attrGetter);
+                    this->registerCallable(setChannelAttrName, attrSetter);
+                    this->registerProbe(getChannelAttrName);
+                }
+            }
+            else
+            {
+                c.disable();
+            }
+        }
+
+        //create sample buffer if we've got any scan elements
+        //buffer size defaults to 4096 samples per buffer, for now
+        if (have_scan_elements) {
+            this->buf = std::unique_ptr<IIOBuffer>(new IIOBuffer(std::move(this->dev->createBuffer(4096, false))));
+            if (!this->buf)
+            {
+                throw Pothos::SystemException("IIOSink::IIOSink()", "buffer creation failed");
+            }
+        }
+    }
+
+    static Block *make(const std::string &deviceId)
+    {
+        return new IIOSink(deviceId);
+    }
+
+    std::string getDeviceAttribute(IIOAttr<IIODevice> a)
+    {
+        return a.value();
+    }
+
+    void setDeviceAttribute(IIOAttr<IIODevice> a, Pothos::Object value)
+    {
+        a = value.toString();
+    }
+
+    std::string getChannelAttribute(IIOAttr<IIOChannel> a)
+    {
+        return a.value();
+    }
+
+    void setChannelAttribute(IIOAttr<IIOChannel> a, Pothos::Object value)
+    {
+        a = value.toString();
+    }
+
+    void work(void)
+    {
+        auto sample_count = this->workInfo().minInElements;
+ 
+        if (this->buf) {
+            //write samples to outputs
+            for (auto c : this->dev->channels())
+            {
+                if (this->allInputs().count(c.id()) > 0) {
+                    auto inputPort = this->input(c.id());
+                    auto inputBuffer = inputPort->buffer();
+
+                    c.write(*this->buf, (void*)inputBuffer.address, sample_count);
+                    inputPort->consume(sample_count);
+                }
+            }
+
+            //push new samples to iio device
+            this->buf->push(sample_count);
+        }
+    }
+};
+
+static Pothos::BlockRegistry registerIIOSink(
+    "/iio/sink", &IIOSink::make);
